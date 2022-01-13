62d217aa7958b575878a527853bb5b561b1b1cde
Maciej Zwoliński <m.zwolinski@satrevolution.com>
2022-01-13 10:42:35 +0100

    Reproduce issue with formatting

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 clang-format version 13.0.0
+
+to reproduce:
+
+```bash
+clang-format --style=file -i main.cpp
+```
---
+++ main_reformated_with_13.0.cpp
@@ -0,0 +1,17 @@
+class Foo {
+  int interface;
+
+  Foo::Foo(int iface) : interface { iface } {
+  }
+}
+
+class Bar {
+  int outerface;
+
+  Bar::Bar(int oface) : outerface{oface} {}
+}
+
+int main() {
+  auto foo = Foo(1);
+  auto bar = Bar(1);
+}
